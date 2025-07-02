4848237ff4a4fdd920c812443139812d75b6b943
mijinn0 <pmj_0818@naver.com>
2025-07-02 14:42:54 +0900

    refactor: v2 API 경로 리팩토링

--- src/apis/constants.ts
+++ src/apis/constants.ts
@@ -1,1 +1,9 @@
 const API_DOMAIN = import.meta.env.REACT_APP_DOMAIN || "http://localhost:8080"; 
+
+const API_BASE = `${API_DOMAIN}/api/v2`
+const AUTH_API = `${API_BASE}/auth`;
+const USER_API = `${API_BASE}/users`;
+const ADMIN_API = `${API_BASE}/admins`;
+const FILE_API = `${API_BASE}/files`;
+const MEMBER_ONE_DAY_TICKET_API = `${MEMBER_API}/me/one-day-tickets`;
+const BOARD_COMMENT_API = (boardId: number) => `${BOARD_API}/${boardId}/comments`;
