45a51d08127431fb408ce405d2a41547c2ba3971
leejunwoo24 <yusm8758@naver.com>
2025-07-13 17:02:27 +0900

    refactor: note

--- src/apis/note/post-note.api.ts
+++ src/apis/note/post-note.api.ts
@@ -3,9 +3,10 @@ import ResponseDto from "@/dtos/response.dto";
 import { axiosInstance, bearerAuthorization, responseErrorHandler, responseSuccessHandler } from "../axiosConfig";
 import { NOTE_API } from "../constants";
 import { AxiosError } from "axios";
-import { GetNoteResponseDto } from "@/dtos/note/response/get-note.response.dto";
+import { postNoteResponseDto } from "@/dtos/note/response/post-note.response.dto";
 
-export const PostNoteRequest = async (accessToken: string,  dto: PostNoteRequestDto ): Promise<ResponseDto<GetNoteResponseDto>> => {
+
+export const PostNoteRequest = async (accessToken: string,  dto: PostNoteRequestDto ): Promise<ResponseDto<postNoteResponseDto>> => {
   try{
     const response = await axiosInstance.post(NOTE_API, dto, bearerAuthorization(accessToken));
 
